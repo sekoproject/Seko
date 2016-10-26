968bf49fb06089aed487b3061acc40f80a803eaf
hdrsd <hdrsd@gmail.com>
2016-10-26 16:09:06 +0900

    Initial commit

---
+++ app/src/main/java/com/sympo/seko/Seko.java
@@ -0,0 +1,13 @@
+package com.sympo.seko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Seko extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seko);
+    }
+}
